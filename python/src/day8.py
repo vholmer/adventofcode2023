084d16f858760a211815a927ab9b3b9d39f50f7a
Viktor Holmér <viktorholmer@gmail.com>
2023-12-16 14:25:09 +0100

    Day 8 done

--- python/src/day8.py
+++ python/src/day8.py
@@ -1,11 +1,22 @@
+import copy
+
+import numpy as np
+
+from typing import (
+    List,
+    Tuple,
+)
+
 class Walker:
     instr: str
     world: dict
     cur_key: str
     num_steps: int
-    pc: str
+    pc: int
+    pc_z: int
+    first_z: str
     in_loop: bool
-    dist_to_z: int
+    z_dists: List[Tuple[str, int]]
     visited: set
 
     def __init__(self, instr: str, world: dict) -> None:
@@ -13,9 +24,11 @@ class Walker:
         self.world = world
         self.cur_key = "AAA"
         self.num_steps = 0
+        self.first_z = None
         self.pc = 0
+        self.pc_z = None
         self.in_loop = False
-        self.dist_to_z = None
+        self.z_dists = set()
         self.visited = set()
 
     def walk(self, verbose = False) -> None:
@@ -26,8 +39,6 @@ class Walker:
             print(f"Reached ZZZ in {self.num_steps} steps.")
 
     def step(self, verbose = False) -> None:
-        self.visited.add(self.cur_key)
-        
         direction = self.instr[self.pc]
 
         left_key = self.world[self.cur_key][0]
@@ -36,20 +47,71 @@ class Walker:
         if direction == "L":
             if left_key in self.visited:
                 self.in_loop = True
-        
             if verbose:
                 print(f"Step {self.num_steps}: At {self.cur_key} going L to {self.world[self.cur_key][0]}")
             self.cur_key = left_key
         elif direction == "R":
             if right_key in self.visited:
                 self.in_loop = True
-        
             if verbose:
                 print(f"Step {self.num_steps}: At {self.cur_key} going R to {self.world[self.cur_key][1]}")
             self.cur_key = right_key
 
+        self.visited.add(self.cur_key)
+
         self.pc = (self.pc + 1) % len(self.instr)
         self.num_steps += 1
+
+        self.check_loop()
+
+    def check_loop(self) -> None:
+        direction = self.instr[self.pc]
+
+        left_key = self.world[self.cur_key][0]
+        right_key = self.world[self.cur_key][1]
+
+        if direction == "L":
+             if left_key in self.visited:
+                self.in_loop = True
+        elif direction == "R":
+            if right_key in self.visited:
+                self.in_loop = True
+
+    def walk_to_z(self) -> int:
+        steps_to_z = 0
+    
+        while True:
+            self.step()
+            steps_to_z += 1
+
+            if self.cur_key.endswith("Z"):
+                break
+
+        if not self.first_z and not self.pc_z:
+            self.first_z = self.cur_key
+            self.pc_z = self.pc
+
+        return steps_to_z
+
+    def walk_z(self) -> None:
+        """Walk normally until Z-key loop"""
+        # While not in first Z-key, walk to next Z-key and build list
+        # if not self.z_dists:
+        while True:
+            steps_to_z = self.walk_to_z()
+        
+            if not self.in_loop:
+                continue
+            
+            from_key = self.cur_key
+
+            in_first_z = self.cur_key == self.first_z and self.pc == self.pc_z
+
+            self.z_dists.add((from_key, steps_to_z))
+
+            if in_first_z:
+                break
+        # Walk to next Z
 
 def solve():
     file = open("data/8/data.txt")
@@ -84,11 +146,19 @@ def solve():
         new_walker = Walker(instr, world)
 
         new_walker.cur_key = starting_point
+        new_walker.visited.add(starting_point)
         
         walkers.append(new_walker)
 
-    while any([x for x in walkers if not x.cur_key.endswith("Z")]):
-        for walker in walkers:
-            walker.step()
+    for walker in walkers:
+        walker.walk_z()
 
-    print(f"Answer 8B: {walkers[0].num_steps}")
+    steps = []
+
+    for walker in walkers:
+        for z_dist in walker.z_dists:
+            steps.append(z_dist[1])
+
+    answer = np.lcm.reduce(steps)
+
+    print(f"Answer 8B: {answer}")
